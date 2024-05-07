cdf6d1579268fc2af03499a7409e35c7499769ed
vboxuser <vboxuser@CSE5006.myguest.virtualbox.org>
2024-05-07 10:46:22 +1000

    Updated the lab 8

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -5,18 +5,31 @@ function Task(props) {
 	console.log(props);
 
 	function onChange() {
-		// Find the task we want to update and update it
-		props.setTasks(tasks => tasks.map(task => {
-			if (task.id === props.id) {
-				return {
-					id: task.id,
-					description: task.description,
-					completed: !task.completed
-				};
-			} else {
-				return task;
-			}
-		}));
+		const updatedTask = {
+			id: props.id,
+			description: props.description,
+			completed: !props.completed
+		};
+		fetch(`http://localhost/api/tasks/${props.id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(updatedTask)
+		})
+		.then(response => response.json())
+		.then(() => {
+			props.setTasks(tasks => tasks.map(task => {
+				if (task.id === props.id) {
+					return updatedTask;
+				} else {
+					return task;
+				}
+			}));
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
 	}
 
 	function onClick() {
@@ -38,7 +51,21 @@ function List(props) {
 	}
 
 	function onClick() {
-		props.setTasks(tasks => [...tasks, { id: tasks.length + 1, description: newTask, completed: false }]);
+		fetch('http://localhost/api/tasks', {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ description: newTask, completed: false })
+		})
+			.then(response => response.json())
+			.then(data => {
+				props.setTasks(tasks => [...tasks, data]);
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+		setNewTask(""); // Clear the input field
 	}
 
 	return (
